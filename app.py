08a2ad9c682905058cae0459d8746268d8fccb41
LakshmiPriya <aradhyulalakshmipriyayadav@gmail.com>
2026-05-17 13:38:56 +0530

    Added feature branch changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "POC-19 Branching Strategy Working"
+    return "POC-19 Branching Strategy Working and Feature Branch Successfully Merged"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
